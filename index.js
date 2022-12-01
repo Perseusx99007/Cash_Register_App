33f9f5d77f095261da81e7f69e650835631df9ee
Perseusx99007 <ratulsaha99@gmail.com>
2022-12-01 22:48:04 +0530

    UPDATE

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const billButton = document.querySelector("#bill-button");
 const checkButton = document.querySelector("#check-button");
+const message = document.queryCommandIndeterm("#error-message");
 
 // billButton.addEventListener("click", () => console.log("clicked", billAmount.value));
 
