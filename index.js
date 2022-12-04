c5d66c552d37fffc802a45189a47b26464e73b75
Perseusx99007 <ratulsaha99@gmail.com>
2022-12-04 21:46:47 +0530

    UPDATE

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ checkButton.addEventListener("click", validateBillAndCashAmount = () =>
         else
         {
             // console.log("The Cash provided should atleast be equal to the Bill Amount.");
-            // message.style.display = "block";
+            message.style.display = "block";
             // message.innerText = "The Cash provided should atleast be equal to the Bill Amount.";
             showMessage("The Cash provided should atleast be equal to the Bill Amount.");
         }
