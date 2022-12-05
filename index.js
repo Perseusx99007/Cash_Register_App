2239e7d9b8266e27581972c02170e3fabab3e389
Perseusx99007 <ratulsaha99@gmail.com>
2022-12-05 21:28:03 +0530

    UPDATE

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const message = document.querySelector("#error-message");
 
 // checkButton.addEventListener("click", () => console.log("CLICKED", cashGiven.value));
 
-function showMessage (message)
+showMessage = (message) =>
 {
     // message.style.display = "block";
     message.innerText = message;
