8fe5c6374c8751ae6508acc38624c6961f300820
Perseusx99007 <ratulsaha99@gmail.com>
2023-01-01 00:11:51 +0530

    UPDATE

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const message = document.querySelector("#error-message");
 
 // billButton.addEventListener("click", () => console.log("clicked", billAmount.value));
 
-checkButton.addEventListener("click", () => console.log("CLICKED", cashGiven.value));
 
+// checkButton.addEventListener("click", () => console.log("CLICKED", cashGiven.value));
 // showMessage = (message) =>
 // {
 //     // message.style.display = "block";
